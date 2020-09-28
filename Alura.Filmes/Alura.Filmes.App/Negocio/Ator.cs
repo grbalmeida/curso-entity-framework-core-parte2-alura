d79184bcf81fbe420c5bd2dd57ccd508a0ae3afc
gilvan <g.r.almeida@live.com>
2020-09-28 20:30:16 -0300

    Mapeando atores

---
+++ Alura.Filmes/Alura.Filmes.App/Negocio/Ator.cs
@@ -0,0 +1,9 @@
+﻿namespace Alura.Filmes.App.Negocio
+{
+    public class Ator
+    {
+        public int Id { get; set; }
+        public string PrimeiroNome { get; set; }
+        public string UltimoNome { get; set; }
+    }
+}
--- Alura.Filmes/Alura.Filmes.App/Program.cs
+++ Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Alura.Filmes.App
+﻿using Alura.Filmes.App.Dados;
+using System;
+
+namespace Alura.Filmes.App
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            using (var contexto = new AluraFilmesContexto())
+            {
+                foreach (var ator in contexto.ConjuntoDeAtores)
+                {
+                    Console.WriteLine(ator);
+                }
+            }
         }
     }
 }
