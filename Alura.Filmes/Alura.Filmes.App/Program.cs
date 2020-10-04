db7da961b1d5ffbb8298a86918c211d81cfda9cd
gilvan <g.r.almeida@live.com>
2020-10-04 13:42:22 -0300

    Usando views com o Entity

--- Alura.Filmes/Alura.Filmes.App/Program.cs
+++ Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -11,7 +11,31 @@ namespace Alura.Filmes.App
     {
         static void Main(string[] args)
         {
-            FromSqlESuasLimitacoes();
+            UsandoViewsComEntity();
+        }
+
+        private static void UsandoViewsComEntity()
+        {
+            using (var contexto = new AluraFilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+
+                var sql = @"select a.*
+                            from actor a
+                            inner join top5_most_starred_actors filmes
+                            on filmes.actor_id = a.actor_id";
+
+                var atoresMaisAtuantes = contexto.Atores
+                    .FromSql(sql)
+                    .Include(a => a.Filmografia);
+
+                foreach (var ator in atoresMaisAtuantes)
+                {
+                    Console.WriteLine($"O ator {ator.PrimeiroNome} {ator.UltimoNome} atuou em {ator.Filmografia.Count} filmes.");
+                }
+            }
+
+            Console.ReadLine();
         }
 
         private static void FromSqlESuasLimitacoes()
