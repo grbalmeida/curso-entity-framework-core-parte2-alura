7ea279461730f3d55cddf2ef1535b389e9d7dca9
gilvan <g.r.almeida@live.com>
2020-10-03 21:50:06 -0300

    O modelo de dados do Entity

--- Alura.Filmes/Alura.Filmes.App/Program.cs
+++ Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Alura.Filmes.App.Dados;
 using Alura.Filmes.App.Extensions;
+using Alura.Filmes.App.Migrations;
 using Alura.Filmes.App.Negocio;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -11,7 +12,34 @@ namespace Alura.Filmes.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("G".ParaValor());
+            ModeloDeDadosDoEntity();
+        }
+
+        private static void ModeloDeDadosDoEntity()
+        {
+            using (var contexto = new AluraFilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+
+                var filme = new Filme
+                {
+                    Titulo = "Casino Royale",
+                    Duracao = 120,
+                    AnoLancamento = "2000",
+                    Classificacao = ClassificacaoIndicativa.MaioresQue14,
+                    IdiomaFalado = contexto.Idiomas.First()
+                };
+
+                contexto.Entry(filme).Property("last_update").CurrentValue = DateTime.Now;
+
+                contexto.Filmes.Add(filme);
+
+                contexto.SaveChanges();
+
+                var filmeInserido = contexto.Filmes.First(f => f.Titulo == "Casino Royale");
+
+                Console.WriteLine(filmeInserido.Classificacao);
+            }
 
             Console.ReadLine();
         }
